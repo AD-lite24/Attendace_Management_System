efd39754b503c7ffdfd9bac417438d165e06ca0c
Aditya Dandwate <adityadandwate24@gmail.com>
2023-04-01 15:28:10 +0530

    some more queries

--- utils.py
+++ utils.py
@@ -36,8 +36,31 @@ def init_schema(connection):
         Dept_name VARCHAR(255) NOT NULL,
         PRIMARY KEY(Course_id),
         FOREIGN KEY(Dept_name) REFERENCES departments(Dept_name)
+        FOREIGN KEY(Emp_id) REFERENCES employees(emp_id)
     )
     """
     )
+
+    #create employee table
+    mycursor.execute(
+        """CREATE TABLE IF NOT EXISTS employees(
+            Emp_id VARCHAR(255) NOT NULL,
+            Name VARCHAR(255) NOT NULL,
+            DOB DATE,
+            PRIMARY KEY (Emp_id);
+        )
+        """
+    )
+
+    #create takes table
+    
+    # mycursor.execute(
+    #     """CREATE TABLE IF NOT EXISTS takes(
+    #         Student_id VARCHAR(255) NOT NULL,
+    #         Course_id VARCHAR(255) NOT NULL,
+    #         Att_date DATE NOT NULL,
+    #     )
+    #     """
+    # )
 
 
