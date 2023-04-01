aa92ba62c292821b17db1b9a76c4f39ab45a18ae
Aditya Dandwate <adityadandwate24@gmail.com>
2023-04-01 15:52:18 +0530

    added more buttons

--- admin_utils.py
+++ admin_utils.py
@@ -13,16 +13,29 @@ class UniversityAdmin():
         new_win.title('Add Student')
         new_win.geometry(self.master.geometry())
 
-        Label(new_win, text='Add Student here', foreground='blue').pack(side= TOP)
+        stu_name = Label(new_win, text='Name:', foreground='green')
+        stu_name.grid(row=0, column=0, pady=5)
 
-        stu_name = Label(new_win, text='Name:', foreground='green').place(relx = 0.1, rely = 0.15, anchor=CENTER)
-        stu_ID = Label(new_win, text='ID:', foreground='green').place(relx = 0.1, rely = 0.24, anchor=CENTER)
+        e_name = Entry(new_win, width=25)
+        e_name.grid(row=0, column=1, pady=5)
 
-        e_name = Entry(new_win, width=25).pack(pady = 1)
-        e_ID = Entry(new_win, width=25).pack(pady=1)
+        stu_ID = Label(new_win, text='ID:', foreground='green')
+        stu_ID.grid(row=1, column=0, pady=5)
 
-        # e_name.place(relx=0.3, rely=0.15, anchor=CENTER)
-        # e_ID.place(relx=0.3, rely=0.24, anchor=CENTER)
+        e_ID = Entry(new_win, width=25)
+        e_ID.grid(row=1, column=1, pady=5)
+
+        stu_dept = Label(new_win, text='Department:', foreground='green')
+        stu_dept.grid(row=2, column=0, pady=5)
+
+        e_dept = Entry(new_win, width=25)
+        e_dept.grid(row=2, column=1, pady=5)
+
+        stu_DOB = Label(new_win, text='DOB:', foreground='green')
+        stu_DOB.grid(row=3, column=0, pady=5)
+
+        e_DOB = Entry(new_win, width=25)
+        e_DOB.grid(row=3, column=1, pady=5)
 
 
     def remove_student():
--- utils.py
+++ utils.py
@@ -35,9 +35,9 @@ def init_schema(connection):
         Emp_id VARCHAR(255), 
         Dept_name VARCHAR(255) NOT NULL,
         PRIMARY KEY(Course_id),
-        FOREIGN KEY(Dept_name) REFERENCES departments(Dept_name)
-        FOREIGN KEY(Emp_id) REFERENCES employees(emp_id)
-    )
+        FOREIGN KEY(Dept_name) REFERENCES departments(Dept_name),
+        FOREIGN KEY(Emp_id) REFERENCES employees(Emp_id)
+    );
     """
     )
 
@@ -47,13 +47,13 @@ def init_schema(connection):
             Emp_id VARCHAR(255) NOT NULL,
             Name VARCHAR(255) NOT NULL,
             DOB DATE,
-            PRIMARY KEY (Emp_id);
-        )
+            PRIMARY KEY (Emp_id)
+        );
         """
     )
 
     #create takes table
-    
+
     # mycursor.execute(
     #     """CREATE TABLE IF NOT EXISTS takes(
     #         Student_id VARCHAR(255) NOT NULL,
