55ba24c2cf6a101415dba2674bdfe1c835c46cf9
Aditya Dandwate <adityadandwate24@gmail.com>
2023-04-06 17:19:51 +0530

    added comments

--- init_schema.sql
+++ init_schema.sql
@@ -1,9 +1,12 @@
+--Create admins
 CREATE TABLE IF NOT EXISTS admins(
             admin_id VARCHAR(255),
             admin_password VARCHAR(255),
             PRIMARY KEY(admin_id)
         );
 
+
+--Initialize admins
 INSERT IGNORE INTO admins
             VALUES
                 ('a1','12345'),
@@ -11,6 +14,7 @@ INSERT IGNORE INTO admins
                 ('a3','12345'),
                 ('a4','98765');
 
+--Create students
 CREATE TABLE IF NOT EXISTS students(
         Student_id VARCHAR(255) NOT NULL,
         First_name VARCHAR(255) NOT NULL,
@@ -19,13 +23,14 @@ CREATE TABLE IF NOT EXISTS students(
         Dept_name VARCHAR(255),
         PRIMARY KEY(Student_id)
     );
-
+--Create departments
 CREATE TABLE IF NOT EXISTS departments(
         Dept_name VARCHAR(255) NOT NULL,
         Dept_location VARCHAR(255),
         PRIMARY KEY(Dept_name)
     );
 
+--Initialize departments
 INSERT IGNORE INTO departments
         VALUES
             ('CSIS','NAB'),
@@ -35,21 +40,24 @@ INSERT IGNORE INTO departments
             ('Eco', 'NAB'),
             ('Bio', 'FD3');
 
+--Create employees
 CREATE TABLE IF NOT EXISTS employees(
             Emp_id VARCHAR(255) NOT NULL,
             Name VARCHAR(255) NOT NULL,
             DOB DATE,
             PRIMARY KEY (Emp_id)
         );
-
+--Create instructors
 CREATE TABLE IF NOT EXISTS instructors(
             Emp_id VARCHAR(255),
             Dept_name VARCHAR(255),
             Password VARCHAR(255),
+            Fav_colour VARCHAR(255),
             PRIMARY KEY (Emp_id),
             FOREIGN KEY (Dept_name) REFERENCES departments(Dept_name)
         );
 
+--Create courses
 CREATE TABLE IF NOT EXISTS courses(
         Course_id VARCHAR(255) NOT NULL,
         Credits int,
@@ -59,7 +67,7 @@ CREATE TABLE IF NOT EXISTS courses(
         FOREIGN KEY(Dept_name) REFERENCES departments(Dept_name),
         FOREIGN KEY(Emp_id) REFERENCES instructors(Emp_id)
     );
-
+--Create takes
 CREATE table if not exists Takes(
         Student_id Varchar(255) NOT NULL,
         Course_id Varchar(255) NOT NULL, 
@@ -70,9 +78,3 @@ CREATE table if not exists Takes(
         FOREIGN KEY(Student_id) REFERENCES Courses(Course_id) on delete cascade on update cascade
     );
 
-CREATE TABLE IF NOT EXISTS employees(
-            Emp_id VARCHAR(255) NOT NULL,
-            Name VARCHAR(255) NOT NULL,
-            DOB DATE,
-            PRIMARY KEY (Emp_id)
-        );
