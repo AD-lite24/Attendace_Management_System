baf7a5525edfde36114fbac7b0d088abc1ad7df2
Aditya Dandwate <adityadandwate24@gmail.com>
2023-04-11 16:38:51 +0530

    changed indentation and spacing;

--- init_schema.sql
+++ init_schema.sql
@@ -24,21 +24,21 @@ CREATE TABLE IF NOT EXISTS departments(
 
 
 CREATE TABLE IF NOT EXISTS employees(
-            Emp_id VARCHAR(255) NOT NULL,
-            First_name VARCHAR(255) NOT NULL,
-            Last_name VARCHAR(255) NOT NULL,
-            DOB DATE,
-            PRIMARY KEY (Emp_id)
-        );
+        Emp_id VARCHAR(255) NOT NULL,
+        First_name VARCHAR(255) NOT NULL,
+        Last_name VARCHAR(255) NOT NULL,
+        DOB DATE,
+        PRIMARY KEY (Emp_id)
+    );
 
 CREATE TABLE IF NOT EXISTS instructors(
-            Emp_id VARCHAR(255),
-            Dept_name VARCHAR(255),
-            Password VARCHAR(255),
-            Fav_colour VARCHAR(255),
-            PRIMARY KEY (Emp_id),
-            FOREIGN KEY (Dept_name) REFERENCES departments(Dept_name)
-        );
+        Emp_id VARCHAR(255),
+        Dept_name VARCHAR(255),
+        Password VARCHAR(255),
+        Fav_colour VARCHAR(255),
+        PRIMARY KEY (Emp_id),
+         FOREIGN KEY (Dept_name) REFERENCES departments(Dept_name)
+    );
 
 
 CREATE TABLE IF NOT EXISTS courses(
@@ -77,9 +77,6 @@ CREATE TABLE IF NOT EXISTS Employee_records(
         PRIMARY KEY(Emp_id, date),
         FOREIGN KEY(Emp_id) REFERENCES employees(Emp_id)
     );
-
-    
-
 
 CREATE TRIGGER tr_up_id_student
 BEFORE INSERT ON students
